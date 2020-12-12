eaa6f1b23b675ef543696ede48106b2e34d848ea
mohammadsheam <mohammadsheam@gmail.com>
2020-12-12 19:41:57 +0600

    day 1

---
+++ basic/day1/script.js
@@ -0,0 +1,33 @@
+ // Data tpyes
+ // Number
+    //console.log(Infinity/Infinity);
+    let variable = "Infinity";
+    //console.log(typeof variable); // string
+    //console.log(variable / 0); // Infinity
+
+    let variable2 = "js";
+    //console.log(typeof variable2); // string
+    //console.log(variable2 / 0); // NaN
+
+
+
+// String methods
+    // search --> indexOf(), search(), lastIndexOf()
+    /* 
+    var str = "count how many \"count\" exist here";
+    console.log(str);
+    var foundOrnot = str.indexOf('sheam');
+    console.log(foundOrnot); // -1
+    var foundOrnot = str.search('sheam');
+    console.log(foundOrnot);
+    */
+
+    let name = "Ilya";
+
+    console.log(`hello ${1}`); // ?
+
+    console.log(`hello ${"name"}`); // ?
+
+    console.log(`hello ${name}`); // ?
+
+//Extracting String Parts-->slice(start, end), substring(start, end), substr(start, length)
